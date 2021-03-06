9c46d4c5a9e20d22cb2849214cd8ef3969f1969e
Pavlos Vinieratos <pvinis@gmail.com>
2021-03-06 15:51:39 +0000

    aha

--- pvinis/scripts/copy-from-og.sh
+++ pvinis/scripts/copy-from-og.sh
@@ -9,21 +9,9 @@ git checkout master
 git pull
 git rev-parse HEAD > $MINE/latest-copy-at-commit.txt
 
-# skip keyboards, only copy over mine
-# skip layouts
-# skip users
-rsync -r $BASE/ $MINE  \
- --exclude='.git'      \
- --exclude='keyboards' \
- --exclude='layouts'   \
- --exclude='users'
-
+# only copy over my files
 # ergodox_ez
-rsync -r $BASE/keyboards/ergodox_ez/ $MINE/keyboards/ergodox_ez \
- --exclude='keymaps'
-# rsync -r $BASE/keyboards/ergodox_ez/keymaps/pvinis/ $MINE/keyboards/ergodox_ez/keymaps/pvinis
+rsync -r $BASE/keyboards/ergodox_ez/keymaps/pvinis/ $MINE/keyboards/ergodox_ez/keymaps/pvinis
 
 # keebio/iris
-rsync -r $BASE/keyboards/keebio/iris/ $MINE/keyboards/keebio/iris \
- --exclude='keymaps'
-# rsync -r $BASE/keyboards/keebio/iris/keymaps/pvinis/ $MINE/keyboards/keebio/iris/keymaps/pvinis
+rsync -r $BASE/keyboards/keebio/iris/keymaps/pvinis/ $MINE/keyboards/keebio/iris/keymaps/pvinis

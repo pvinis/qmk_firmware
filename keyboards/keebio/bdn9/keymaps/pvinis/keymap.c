388a9c16ee35fa7ca85a514146a502c3516e7414
Pavlos Vinieratos <pvinis@gmail.com>
2021-03-24 17:08:32 +0000

    more

--- keyboards/keebio/bdn9/keymaps/pvinis/keymap.c
+++ keyboards/keebio/bdn9/keymaps/pvinis/keymap.c
@@ -20,6 +20,36 @@ enum encoder_names {
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    // ┌────┬────┬────┐
+    // │    │MUTE│    │
+    // ├────┼────┼────┤
+    // │  1 │roro│  2 │
+    // ├────┼────┼────┤
+    // │PREV│PLAY│NEXT│
+    // └────┴────┴────┘
+    [LR_BASE] = LAYOUT(
+        _______      , KC_MUTE   , _______,
+        MO(LR_SYMBOL), HYPR(KC_8), MO(LR_SYSCTL),
+        KC_MPRV      , KC_MPLY   , KC_MNXT
+    ),
+
+    // └────┴────┴────┘
+    [LR_SYSCTL] = LAYOUT(
+    ),
+    // ┌────┬────┬────┐
+    // │    │    │    │
+    // ├────┼────┼────┤
+    // │    │    │    │
+    // ├────┼────┼────┤
+    // │VRSN│MAKE│RST │
+    // └────┴────┴────┘
+    [LR_KBCTL] = LAYOUT(
+        XXXXXXX, XXXXXXX, XXXXXXX,
+        _______, XXXXXXX, _______,
+        PV_VRSN, PV_MAKE, PV_RST
+    ),
+};
+
 #ifdef ENCODER_ENABLE
 void encoder_update_user(uint8_t index, bool clockwise) {
     if (index == ROT_LEFT) {

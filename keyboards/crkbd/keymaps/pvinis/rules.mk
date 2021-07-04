91d80da15ca71d5aab6a98a784b980d924fdb623
Pavlos Vinieratos <pvinis@gmail.com>
2021-07-04 09:13:58 +0100

    init

---
+++ keyboards/crkbd/keymaps/pvinis/rules.mk
@@ -0,0 +1,6 @@
+# According to
+# https://github.com/ItsWaffIe/waffle_microdox/wiki/Microdox-Build-Guide#cli-more-advanced
+# I should use dfu for elite-c, which I have.
+BOOTLOADER = qmk-dfu
+
+OLED_DRIVER_ENABLE  = yes
